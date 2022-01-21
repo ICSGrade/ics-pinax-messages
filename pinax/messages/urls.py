c9bfa6f98fbcaface764ae2d996eaf7664cceb4e
ICSGrade <94392586+ICSGrade@users.noreply.github.com>
2022-01-21 10:17:19 -0800

    Updated urls.py to Django 4.0 compliant
    Changed 'url' to 're_path' as required by Django 4.0

--- pinax/messages/urls.py
+++ pinax/messages/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = "pinax_messages"
 
 urlpatterns = [
-    url(r"^inbox/$", views.InboxView.as_view(),
+    re_path(r"^inbox/$", views.InboxView.as_view(),
         name="inbox"),
-    url(r"^create/$", views.MessageCreateView.as_view(),
+    re_path(r"^create/$", views.MessageCreateView.as_view(),
         name="message_create"),
-    url(r"^create/(?P<user_id>\d+)/$", views.MessageCreateView.as_view(),
+    re_path(r"^create/(?P<user_id>\d+)/$", views.MessageCreateView.as_view(),
         name="message_user_create"),
-    url(r"^thread/(?P<pk>\d+)/$", views.ThreadView.as_view(),
+    re_path(r"^thread/(?P<pk>\d+)/$", views.ThreadView.as_view(),
         name="thread_detail"),
-    url(r"^thread/(?P<pk>\d+)/delete/$", views.ThreadDeleteView.as_view(),
+    re_path(r"^thread/(?P<pk>\d+)/delete/$", views.ThreadDeleteView.as_view(),
         name="thread_delete"),
 ]
